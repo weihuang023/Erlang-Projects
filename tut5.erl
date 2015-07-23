aca5bbddc6601729623742d69c1f45ae10e3009d
Wei Huang <weih81888@gmail.com>
2015-07-23 17:54:27 -0400

    Update tut5.erl

--- tut5.erl
+++ tut5.erl
@@ -5,15 +5,15 @@
 
 format_temps([])->                        % No output for an empty list
     ok;
-format_temps([City | Rest]) ->
-    print_temp(convert_to_celsius(City)),
-    format_temps(Rest).
+format_temps([City | Rest]) ->            
+    print_temp(convert_to_celsius(City)), % when format temp is called the first time, city gets the value and print temp is called 
+    format_temps(Rest).                   % Rest is the rest of list and print temp is called
 
 convert_to_celsius({Name, {c, Temp}}) ->  % No conversion needed
     {Name, {c, Temp}};
 convert_to_celsius({Name, {f, Temp}}) ->  % Do the conversion
     {Name, {c, (Temp - 32) * 5 / 9}}.
 
-print_temp({Name, {c, Temp}}) ->
+print_temp({Name, {c, Temp}}) ->          % print tem is simple call io:format
     io:fwrite("~-15w ~w~n", [cityName, temperature]),
     io:format("~-15w ~w c~n", [Name, Temp]).
