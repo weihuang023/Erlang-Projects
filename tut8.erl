f2a153e21bd040c8f298612225247db5a2875cbb
Wei Huang <weih81888@gmail.com>
2015-07-23 18:36:54 -0400

    Update tut8.erl

--- tut8.erl
+++ tut8.erl
@@ -2,11 +2,16 @@
 
 -export([reverse/1]).
 
-reverse(List) ->
-    reverse(List, []).
+reverse(List) ->       % reverse([1,2,3])
+    reverse(List, []). % reverse([1,2,3],[])
 
-reverse([Head | Rest], Reversed_List) ->
-    reverse(Rest, [Head | Reversed_List]);
+reverse([Head | Rest], Reversed_List) ->   % reverse([1|2,3],[])
+    reverse(Rest, [Head | Reversed_List]); % reverse([2,3],[1|[]])
+                                           % reverse([2|3],[1])
+                                           % reverse([3],[2|[1]])
+                                           % reverse([3|[]],[2,1])
+                                           % reverse([],[3|[2,1]])
+                                           % reverse([],[3,2,1])
 
-reverse([], Reversed_List) ->
-    Reversed_List.
+reverse([], Reversed_List) ->            % reverse([],[3,2,1]) 
+    Reversed_List.                       % [3,2,1]
