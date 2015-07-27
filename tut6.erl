c30e19e745dc9c9437dde594da9bb22bb09a1747
Wei Huang <weih81888@gmail.com>
2015-07-27 15:22:28 -0400

    Update tut6.erl

--- tut6.erl
+++ tut6.erl
@@ -3,12 +3,13 @@
 %% There are two functions with same name list_max
 %% Arity is number of arguments in this case list_max/1 and list_max/2.
 list_max([Head|Rest]) -> 
-   list_max(Rest, Head). % Swap rest number with head number 
-                         % list_max([2,3,4,5,7,4,3,2,1],1) when list_max([1,2,3,4,5,7,4,3,2,1]).
+   list_max(Rest, Head).                                          % Swap rest number with head number 
+                                              % list_max([2,3,4,5,7,4,3,2,1],1) when list_max([1,2,3,4,5,7,4,3,2,1]).
 
-list_max([], Res) -> % result will end when list is empty 
+list_max([], Res) ->                                              % result will end when list is empty on the head
     Res;             
 list_max([Head|Rest], Result_so_far) when Head > Result_so_far -> % to find the result so far use the when before "->" - guard  
     list_max(Rest, Head);                                         % Usefull operation such > < >= <= == in guard
-list_max([Head|Rest], Result_so_far)  ->
+list_max([Head|Rest], Result_so_far)  ->                          % (Rest,Head) -> ([Head | Rest], Result_so_far) 
+                                                                  % result so far is head from the provious list_max                                                                  % ([],Res) when list is 
     list_max(Rest, Result_so_far).
