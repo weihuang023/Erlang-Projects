979fff3b17b0b3ae66baeeeb21d72207e9888348
Wei Huang <weih81888@gmail.com>
2015-07-27 16:01:55 -0400

    Update tut13.erl

--- tut13.erl
+++ tut13.erl
@@ -8,6 +8,6 @@ convert_to_c({Name, {c, Temp}}) ->
     {Name, {c, Temp}}.
 
 convert_list_to_c(List) ->
-    New_list = lists:map(fun convert_to_c/1, List),
-    lists:sort(fun({_, {c, Temp1}}, {_, {c, Temp2}}) ->
+    New_list = lists:map(fun convert_to_c/1, List), % map the function and lists of element together
+    lists:sort(fun({_, {c, Temp1}}, {_, {c, Temp2}}) -> % create the new list has condition with temp1 and temp2
                        Temp1 < Temp2 end, New_list).
